3387086e9aab73ed84e93b522704b28df0bb1f16
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2021-07-10 02:30:09 +0200

    [client][TodoList] start design

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./Page/TodoList";
+import GlobalStyle from "./Page/GlobalStyle";
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Page/TodoList.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+import Task from '../Component/TaskItem/Task'
+function TodoList() {
+    return (
+        <Fragment>
+            <Task/>
+            <Task/>
+            <Task/>
+            <Task/>
+        </Fragment>
+    )
+}
+
+export default TodoList
